0c9fe35da752683c5129143cabb8b62f152201b5
BrokenDuskL <brokenduskl@gmail.com>
2026-08-01 10:42:28 +0800

    test: cover typed environment values
    Signed-off-by: BrokenDuskL <brokenduskl@gmail.com>

--- tests/ut/test_envs.py
+++ tests/ut/test_envs.py
@@ -38,10 +38,12 @@ class TestEnvVariables(TestBase):
                                      var_handler())
 
                     handler_source = inspect.getsource(var_handler)
-                    if 'int(' in handler_source:
-                        test_vals = ["123", "456"]
-                    elif 'bool(int(' in handler_source:
+                    if 'bool(int(' in handler_source:
                         test_vals = ["0", "1"]
+                    elif 'int(' in handler_source:
+                        test_vals = ["123", "456"]
+                    elif 'float(' in handler_source:
+                        test_vals = ["1.25", "2.5"]
                     else:
                         test_vals = [f"test_{var_name}", f"custom_{var_name}"]
 
